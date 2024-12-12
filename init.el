0d8d4505f18214aa3f81e30ce65bcb501f4c4e88
ben <benwolthuis@jaamo.nl>
2024-12-12 21:04:32 +0100

    stacked up config tweaks from work

--- init.el
+++ init.el
@@ -8,10 +8,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(flycheck-checker-error-threshold 1000)
+ '(js-indent-level 2)
  '(magit-display-buffer-function 'magit-display-buffer-fullcolumn-most-v1))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(magit-diff-base ((t (:extend t :background "#d08770" :foreground "dark red"))))
+ '(magit-diff-base-highlight ((t (:extend t :background "#d08770" :foreground "dark red" :weight bold)))))
